43a17e78356a11efb8f50ca2434fdd254b31fcbe
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2017-10-03 13:55:56 -0300

    Versionando

---
+++ prmToolkit.Configuration/prmToolkit.Configuration/Configuration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace prmToolkit.Configuration
+{
+    public class Configuration
+    {
+        public static string GetKeyAppSettings(string key)
+        {
+            var value = ConfigurationManager.AppSettings[key];
+
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new Exception($"Chave '{key}' não definida na tag APPSETTINGS. Verifique seu WebConfig ou AppConfig.");
+            }
+
+            return value;
+        }
+
+        public static string GetConnectionString(string key)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings[key]?.ConnectionString;
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new Exception($"Chave '{key}' não definida na tag CONNECTIONSTRINGS. Verifique seu WebConfig ou AppConfig.");
+            }
+
+            return connectionString;
+        }
+    }
+}
